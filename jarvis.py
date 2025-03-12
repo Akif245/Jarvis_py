c69a53d380d62737da2a9f145699099a9b015f94
Akif245 <abdulakif44570@gmail.com>
2025-03-13 01:38:20 +0530

    Jarvis project

--- jarvis.py
+++ jarvis.py
@@ -1,11 +1,38 @@
 import pyttsx3
-import speech_recognition as sr 
-import webbrowser 
+import speech_recognition as sr
+import webbrowser
 import datetime
-import pyjokes 
+import pyjokes
 
-def sptext():
-    recog=sr.Recognizer()
+def sptext():   
+    recog = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listining !")
-        recog.adjust_for_ambient_noise() 
+        print("Listening!")
+        recog.adjust_for_ambient_noise(source)  
+        audio = recog.listen(source)
+        try:
+            print("Recognizing...")
+            data = recog.recognize_google(audio)
+            print(data)
+            return data
+        except sr.UnknownValueError:
+            print("Could not understand the audio")
+
+sptext()
+def speechtx(x):
+    obj=pyttsx3.init()
+    voice=obj.getProperty('voices')
+    obj.setProperty('voice',voice[1].id)
+    rate=obj.getProperty('rate')
+    obj.setProperty('rate',150)
+    obj.say(x)
+    obj.runAndWait()
+
+#speechtx("Hii bro what r u doing ")
+
+"""if __name__=='__main__':
+        if sptext().lower() =="hello":
+            pass
+        else:
+             print("tata")
+"""
